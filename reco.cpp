7986d1058858ce8ebd289934d957587a25a52698
Saleh1204 <stu2@rlk20.pc.ccse.kfupm.edu.sa>
2016-04-28 11:49:20 +0300

    Adaptive Thresholding Threshold is not optimized

--- reco.cpp
+++ reco.cpp
@@ -6,31 +6,31 @@
 #include <fstream>
 #include <sstream>
 #include <ostream>
-
+#include <string>
 using namespace cv;
 using namespace std;
 
 namespace Color {
     enum Code {
-            FG_RED      = 31,
-                    FG_GREEN    = 32,
-                            FG_BLUE     = 34,
-                                    FG_DEFAULT  = 39,
-                                            BG_RED      = 41,
-                                                    BG_GREEN    = 42,
-                                                            BG_BLUE     = 44,
-                                                                    BG_DEFAULT  = 49
-                                                                        };
-                                                                            class Modifier {
-                                                                                    Code code;
-                                                                                        public:
-                                                                                                Modifier(Code pCode) : code(pCode) {}
-                                                                                                        friend std::ostream&
-                                                                                                                operator<<(std::ostream& os, const Modifier& mod) {
-                                                                                                                            return os << "\033[" << mod.code << "m";
-                                                                                                                                    }
-                                                                                                                                        };
-                                                                                                                                        }
+	FG_RED      = 31,
+        FG_GREEN    = 32,
+        FG_BLUE     = 34,
+        FG_DEFAULT  = 39,
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+    class Modifier {
+        Code code;
+        public:
+               Modifier(Code pCode) : code(pCode) {}
+    	       friend std::ostream&
+	       operator<<(std::ostream& os, const Modifier& mod) {
+               return os << "\033[" << mod.code << "m";
+    	       }
+	};
+}
 
 
 static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
@@ -54,7 +54,7 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
 int main(int argc, const char *argv[]) {
     // Check for valid command line arguments, print usage
     // if no arguments were given.
-    if (argc != 3) {
+    if (argc < 3) {
         cout << "usage: " << argv[0] << " <csvTraining.ext> <csvTesting.ext> " << endl;
         exit(1);
     }
@@ -92,6 +92,12 @@ int main(int argc, const char *argv[]) {
     // later in code to reshape the images to their original
     // size:
     int height = images[0].rows;
+//	std::string::size_type sz;     // alias of size_t
+    double threshold = 50;
+    if (argc == 4)
+    {
+    	threshold = atof(argv[3]);//std::stod(argv[3], &sz);
+    }
 	
     // The following lines create an LBPH model for
     // face recognition and train it with the images and
@@ -116,58 +122,66 @@ int main(int argc, const char *argv[]) {
     //      cv::createLBPHFaceRecognizer(1,8,8,8,123.0)
     //
 	
-	// Training Stage
+	double oldAcc =0, newAcc = 1, change = 3;
+		// Training Stage
 	cout << "Training The Recognizer with "<<images.size()<<" images \nIt may take some time" << endl;
 	Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
-    model->train(images, labels);
+	model->train(images, labels);
 	
-	
-	// Setting Threshold 
-	model->set("threshold",64);
-	
-	
-	// Testing Stage
-	int mispredicted = 0;
-	int unpredicted = 0;
-	int predictedLabel = 0;
-	double confidence = 0;
-	cout << "Start testing " << labelsTesting.size() << " images one by one"<< endl;
-	//#pragma omp parallel for ordered schedule(dynamic) //num_threads(8)
-	for (int i=0; i<labelsTesting.size();i++)
+	while (abs(newAcc - oldAcc) > 0.3)
 	{
-		//predictedLabel = model->predict(imagesTesting[i]);
-		//
-		// To get the confidence of a prediction call the model with:
-		//
-		//      int predictedLabel = -1;
-		//      double confidence = 0.0;
-		model->predict(imagesTesting[i], predictedLabel, confidence);
-		//
-		if (predictedLabel == -1)
+		// Setting Threshold 
+		model->set("threshold",threshold);
+	
+	
+		// Testing Stage
+		int mispredicted = 0;
+		int unpredicted = 0;
+		int predictedLabel = 0;
+		double confidence = 0;
+		cout << "Start testing " << labelsTesting.size() << " images one by one with threshold = "<< threshold << endl;
+		for (int i=0; i<labelsTesting.size();i++)
 		{
-			unpredicted++;
-			confidence = -1.0;
+			//predictedLabel = model->predict(imagesTesting[i]);
+			//
+			// To get the confidence of a prediction call the model with:
+			//
+			//      int predictedLabel = -1;
+			//      double confidence = 0.0;
+			model->predict(imagesTesting[i], predictedLabel, confidence);
+			//
+			if (predictedLabel == -1)
+			{
+				unpredicted++;
+				confidence = -1.0;
+			}
+			if (predictedLabel != labelsTesting[i] && predictedLabel != -1)
+			{
+				mispredicted++;
+			}
+			string result_message = format("Image : %03d   Predicted class = %02d / Actual class = %02d / with Confidence %02.3f.", i,predictedLabel,labelsTesting[i], confidence);
+			cout << result_message << endl;
 		}
-		if (predictedLabel != labelsTesting[i] && predictedLabel != -1)
-		{
-			mispredicted++;
-		}
-		string result_message = format("Image : %03d   Predicted class = %02d / Actual class = %02d / with Confidence %02.3f.", i,predictedLabel,labelsTesting[i], confidence);
-		cout << result_message << endl;
+		cout << "Unrecognized : " << unpredicted << endl; cout << "Mispredicted : " << mispredicted << endl; 
+		double accuracy = double (labelsTesting.size() - unpredicted);
+		accuracy = accuracy - mispredicted;
+		accuracy = accuracy / (1.0*labelsTesting.size());
+		accuracy = accuracy * 100.00;
+		//(((labelsTesting.size()-(unpredicted+mispredicted))/labelsTesting.size()) * 100.0); 
+		string acc = format("Number of Test Subjects is : %d\tNumber of Test Images is : %d \nAccuracy is %.3f %%", (labelsTesting[labelsTesting.size()-1] + 1),labelsTesting.size(),accuracy); 
+		// Sometimes you'll need to get/set internal model data,
+		Color::Modifier red(Color::FG_RED);
+		Color::Modifier green(Color::FG_GREEN);
+		Color::Modifier blue(Color::FG_BLUE);
+		Color::Modifier def(Color::FG_DEFAULT);
+		cout << green << acc << def << endl;
+		oldAcc = newAcc;
+		newAcc = accuracy;
+		if (unpredicted >= mispredicted)
+			threshold = threshold + change;
+		else
+			threshold = threshold - change;
 	}
-	cout << "Unrecognized : " << unpredicted << endl; cout << "Mispredicted : " << mispredicted << endl; 
-	double accuracy = double (labelsTesting.size() - unpredicted);
-	accuracy = accuracy - mispredicted;
-	accuracy = accuracy / (1.0*labelsTesting.size());
-	accuracy = accuracy * 100.00;
-	//(((labelsTesting.size()-(unpredicted+mispredicted))/labelsTesting.size()) * 100.0); 
-	string acc = format("Number of Test Subjects is : %d\tNumber of Test Images is : %d \nAccuracy is %.3f %%", (labelsTesting[labelsTesting.size()-1] + 1),labelsTesting.size(),accuracy); 
-	// Sometimes you'll need to get/set internal model data,
-	Color::Modifier red(Color::FG_RED);
-	Color::Modifier green(Color::FG_GREEN);
-	Color::Modifier blue(Color::FG_BLUE);
-	Color::Modifier def(Color::FG_DEFAULT);
-	cout << green << acc << def << endl;
 	    // which isn't exposed by the public cv::FaceRecognizer.
     // Since each cv::FaceRecognizer is derived from a
     // cv::Algorithm, you can query the data.
