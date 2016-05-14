35b0acc98bd395b6775f68d73b5f3349541369cf
Saleh <saleh.lehigh@gmail.com>
2016-05-14 16:34:23 +0300

    Presented Project GUI Application is working

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -19,22 +19,27 @@
 using namespace cv;
 using namespace std;
 
+
+
 // These vectors hold the images and corresponding labels.
 vector<Mat> images, imagesTesting;
 vector<int> labels, labelsTesting;
+vector<string> trainingPath, testingPath;
 Ptr<FaceRecognizer> model;
 double myThreshold = 100;
 // Testing Variables
-int mispredicted = 0;
+int mispredicted = 0; // You can acess the label by typing ui->mispredicted_lbl and the other values are the same
 int unpredicted = 0;
 int predictedLabel = 0;
 double confidence = 0;
 int totalTestingImageNumber = 0;
 int testIndex = 0;
-static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+
+static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator, vector<string>& paths) {
     std::ifstream file(filename.c_str(), ifstream::in);
     if (!file) {
         string error_message = "No valid input file was given, please check the given filename.";
+
         CV_Error(CV_StsBadArg, error_message);
     }
     string line, path, classlabel;
@@ -45,6 +50,7 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
         if(!path.empty() && !classlabel.empty()) {
             images.push_back(imread(path, 0));
             labels.push_back(atoi(classlabel.c_str()));
+			paths.push_back(path);
         }
     }
 }
@@ -75,62 +81,110 @@ void MainWindow::on_uploadTest_clicked()
                                                     | QFileDialog::DontResolveSymlinks);
 													*/
     //Dir is the path of folder
-	QString filename = QFileDialog::getOpenFileName(this, tr("Open Testing File"), "/home", tr("CSV Files (*.CSV)"));
+	QString filename = QFileDialog::getOpenFileName(this, tr("Open Testing File"), "/home/pi/Desktop/FaceRecognitionProject/GUI", tr("CSV Files (*.CSV)"));
 	std::string sfilename = filename.toUtf8().constData();
 	std::cout << sfilename << std::endl;
+	try {
+        read_csv(sfilename, imagesTesting, labelsTesting, ';',testingPath);
+    } catch (cv::Exception& e) {
+        cerr << "Error opening file \"" << sfilename << "\". Reason: " << e.msg << endl;
+        // nothing more we can do
+        exit(1);
+    }
 	totalTestingImageNumber = labelsTesting.size();
 
+    ui->status_lbl->setText("Status: Done");
+	QString total = "Total Number of Tested Images: "+ QString::number(totalTestingImageNumber) ;
+	ui->totalTestingImageNumber_lbl->setText(total);
 
 
 
 }
 
+QString pl;
+QString con;
+QString unp;
+QString mis;
 void MainWindow::on_testBt_clicked()
 {
 // here shuld be the Recognizer
     //Now create an image
-   // QImage image(dir+"/2.png");
-   // QGraphicsPixmapItem* item = new QGraphicsPixmapItem(QPixmap::fromImage(image));
+    //QImage image(testingPath[testIndex]);
+	string imPath = testingPath[testIndex];
+	QImage image(imPath.c_str());
+    QGraphicsPixmapItem* item = new QGraphicsPixmapItem(QPixmap::fromImage(image));
 
     // add the image to a scene
-   // QGraphicsScene* scene = new QGraphicsScene();
-   // scene->addItem(item);
+    QGraphicsScene* scene = new QGraphicsScene();
+    scene->addItem(item);
     // add the scene to the image viewer
-   // ui->img1->setScene(scene);
+    ui->img1->setScene(scene);
+
    // Testing 
+
+    ui->status_lbl->setText("Status: Testing...");
+
 	model->predict(imagesTesting[testIndex], predictedLabel, confidence);
    // Ready
+
+    ui->status_lbl->setText("Status: Ready");
+
+
 	if (predictedLabel == -1)
 	{
 		unpredicted++;
+         unp= "Unpredicted: "+ QString::number(unpredicted) ;
+        ui-> unpredicted_lbl->setText(unp);
 		confidence = -1.0;
 	}
 	else if (predictedLabel != labelsTesting[testIndex] && predictedLabel != -1)
 	{
-		mispredicted++;
+        mispredicted++;
+         
 		
 	}
 	else
 	{
 		// Display images
+		int pos = std::find(labels.begin(), labels.end(), predictedLabel) - labels.begin();
 		//int* imgIndex = std::find(labels.begin(), labels.end(), &predictedLabel) ;
-		//cout << *imgIndex << endl;
+		cout << pos << "\t Path: " << trainingPath[pos] <<endl;
+		string imPath2 = trainingPath[pos];
+		QImage image2(imPath2.c_str());
+		QGraphicsPixmapItem* item2 = new QGraphicsPixmapItem(QPixmap::fromImage(image2));
+
+		// add the image to a scene
+		QGraphicsScene* scene2 = new QGraphicsScene();
+		scene2->addItem(item2);
+    // add the scene to the image viewer
+		ui->img2->setScene(scene2);
 		//images[imgIndex];
 	}	
-	
-	
+	mis= "Mispredicted: "+ QString::number(mispredicted) ;
+    ui-> mispredicted_lbl->setText(mis);
+	unp= "Unpredicted: "+ QString::number(unpredicted) ;
+    ui-> unpredicted_lbl->setText(unp);
 	testIndex++;
+   // ui->label= ui->label+stTest;
+
+     pl= "Predicted Label: "+ QString::number(predictedLabel) ;
+    ui->predictedLabel_lbl->setText(pl);
+
+     con= "Confidence: "+ QString::number(confidence) ;
+    ui->confidence_lbl->setText(con);
+
 }
 
 void MainWindow::on_uploadTraning_clicked()
 {
     //refrence http://doc.qt.io/qt-4.8/qfiledialog.html#QFileDialog
 
-	QString filename = QFileDialog::getOpenFileName(this, tr("Open Training File"), "/home", tr("CSV Files (*.CSV)"));
+	QString filename = QFileDialog::getOpenFileName(this, tr("Open Training File"), "/home/pi/Desktop/FaceRecognitionProject/GUI", tr("CSV Files (*.CSV)"));
 	std::string sfilename = filename.toUtf8().constData();
 	std::cout << sfilename << std::endl;
+    ui->status_lbl->setText("Status: Done");
 	try {
-        read_csv(sfilename, images, labels);
+        read_csv(sfilename, images, labels, ';',trainingPath);
     } catch (cv::Exception& e) {
         cerr << "Error opening file \"" << sfilename << "\". Reason: " << e.msg << endl;
         // nothing more we can do
@@ -138,12 +192,17 @@ void MainWindow::on_uploadTraning_clicked()
     }
 	model = createLBPHFaceRecognizer();
 	// Label is Waiting (training)
+    ui->status_lbl->setText("Status: Wait.. training..");
 	model->train(images, labels);
 	cout << "Done Training" << endl;
 	// Label is (Ready )
+    ui->status_lbl->setText("Status: Ready");
 	model->set("threshold",myThreshold);
 	
 }
+
+
+
 
 // The method below not important, but Deleting will cause an error
 void MainWindow::on_img1_rubberBandChanged(const QRect &viewportRect, const QPointF &fromScenePoint, const QPointF &toScenePoint)
--- GUI/ui_mainwindow.h
+++ GUI/ui_mainwindow.h
@@ -36,6 +36,12 @@ public:
     QGraphicsView *img1;
     QGraphicsView *img2;
     QLabel *label;
+    QLabel *status_lbl;
+    QLabel *mispredicted_lbl;
+    QLabel *unpredicted_lbl;
+    QLabel *predictedLabel_lbl;
+    QLabel *confidence_lbl;
+    QLabel *totalTestingImageNumber_lbl;
     QMenuBar *menuBar;
     QMenu *menuFace_Recognition_for_COE_487;
     QToolBar *mainToolBar;
@@ -45,18 +51,18 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(765, 701);
+        MainWindow->resize(770, 705);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         uploadTest = new QPushButton(centralWidget);
         uploadTest->setObjectName(QString::fromUtf8("uploadTest"));
-        uploadTest->setGeometry(QRect(70, 580, 141, 27));
+        uploadTest->setGeometry(QRect(160, 590, 141, 27));
         testBt = new QPushButton(centralWidget);
         testBt->setObjectName(QString::fromUtf8("testBt"));
-        testBt->setGeometry(QRect(340, 580, 99, 27));
+        testBt->setGeometry(QRect(370, 590, 99, 27));
         uploadTraning = new QPushButton(centralWidget);
         uploadTraning->setObjectName(QString::fromUtf8("uploadTraning"));
-        uploadTraning->setGeometry(QRect(530, 580, 151, 27));
+        uploadTraning->setGeometry(QRect(530, 590, 151, 27));
         img1 = new QGraphicsView(centralWidget);
         img1->setObjectName(QString::fromUtf8("img1"));
         img1->setGeometry(QRect(20, 80, 351, 361));
@@ -65,14 +71,32 @@ public:
         img2->setGeometry(QRect(405, 80, 341, 361));
         label = new QLabel(centralWidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(300, 20, 181, 41));
+        label->setGeometry(QRect(300, 20, 200, 50));
         QFont font;
         font.setPointSize(17);
         label->setFont(font);
+        status_lbl = new QLabel(centralWidget);
+        status_lbl->setObjectName(QString::fromUtf8("status_lbl"));
+        status_lbl->setGeometry(QRect(10, 620, 371, 17));
+        mispredicted_lbl = new QLabel(centralWidget);
+        mispredicted_lbl->setObjectName(QString::fromUtf8("mispredicted_lbl"));
+        mispredicted_lbl->setGeometry(QRect(40, 450, 261, 17));
+        unpredicted_lbl = new QLabel(centralWidget);
+        unpredicted_lbl->setObjectName(QString::fromUtf8("unpredicted_lbl"));
+        unpredicted_lbl->setGeometry(QRect(40, 480, 251, 17));
+        predictedLabel_lbl = new QLabel(centralWidget);
+        predictedLabel_lbl->setObjectName(QString::fromUtf8("predictedLabel_lbl"));
+        predictedLabel_lbl->setGeometry(QRect(40, 510, 251, 17));
+        confidence_lbl = new QLabel(centralWidget);
+        confidence_lbl->setObjectName(QString::fromUtf8("confidence_lbl"));
+        confidence_lbl->setGeometry(QRect(40, 570, 241, 17));
+        totalTestingImageNumber_lbl = new QLabel(centralWidget);
+        totalTestingImageNumber_lbl->setObjectName(QString::fromUtf8("totalTestingImageNumber_lbl"));
+        totalTestingImageNumber_lbl->setGeometry(QRect(40, 540, 281, 17));
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 765, 25));
+        menuBar->setGeometry(QRect(0, 0, 770, 25));
         menuFace_Recognition_for_COE_487 = new QMenu(menuBar);
         menuFace_Recognition_for_COE_487->setObjectName(QString::fromUtf8("menuFace_Recognition_for_COE_487"));
         MainWindow->setMenuBar(menuBar);
@@ -97,6 +121,12 @@ public:
         testBt->setText(QApplication::translate("MainWindow", "Match", 0, QApplication::UnicodeUTF8));
         uploadTraning->setText(QApplication::translate("MainWindow", "Upload training set", 0, QApplication::UnicodeUTF8));
         label->setText(QApplication::translate("MainWindow", "Face Recognition", 0, QApplication::UnicodeUTF8));
+        status_lbl->setText(QApplication::translate("MainWindow", "Status: TEST", 0, QApplication::UnicodeUTF8));
+        mispredicted_lbl->setText(QApplication::translate("MainWindow", "Mispredicted", 0, QApplication::UnicodeUTF8));
+        unpredicted_lbl->setText(QApplication::translate("MainWindow", "Unpredicted", 0, QApplication::UnicodeUTF8));
+        predictedLabel_lbl->setText(QApplication::translate("MainWindow", "Predicted Label", 0, QApplication::UnicodeUTF8));
+        confidence_lbl->setText(QApplication::translate("MainWindow", "Confidence", 0, QApplication::UnicodeUTF8));
+        totalTestingImageNumber_lbl->setText(QApplication::translate("MainWindow", "Total Number of Tested Images ", 0, QApplication::UnicodeUTF8));
         menuFace_Recognition_for_COE_487->setTitle(QApplication::translate("MainWindow", "Face Recognition for COE 487", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
